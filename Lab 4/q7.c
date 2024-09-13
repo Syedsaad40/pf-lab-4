2305a704f9cf989c38edb010c21c22343a140e73
Syedsaad40 <mosyedsaad11@gmail.com>
2024-09-14 00:34:50 +0500

    Add files via upload

---
+++ Lab 4/q7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+printf("             ___________________________      \n");
+printf("            /                          \\         \n ");
+printf("          /   /\\_/\\                   \\        \n");
+printf("          /   (  . . )                   \\            \n");
+printf("         /     )    (                     \\           \n");
+printf("\"cat\"  |       ( < > )                     |           \n");
+printf("         \\      ----                      /         \n");
+printf("          \\   K24-0589                   /          \n");
+printf("           \\                            /            \n");
+printf("            \\__________________________/            \n");
+
+
+return 0;	
+	
+}
