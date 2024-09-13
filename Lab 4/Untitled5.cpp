9a93340464e02b864c71ad61e1501e2f98da4a5d
Syedsaad40 <mosyedsaad11@gmail.com>
2024-09-13 20:39:10 +0500

    Add files via upload

---
+++ Lab 4/Untitled5.cpp
@@ -0,0 +1,73 @@
+#include<stdio.h>
+int main (){
+	int num ;
+	printf("Enter the positive number: ");
+	scanf("%d",&num);
+	if (num < 0 && num > 20 ){
+		 printf("Invalid number entered");
+	}
+	switch (num){
+		case 1:
+			printf("The number is 1");
+			break;
+			case 2:
+			printf("The number is 2");
+			break;
+			case 3:
+			printf("The number is 3");
+			break;
+			case 4:
+			printf("The number is 4");
+			break;
+			case 5:
+			printf("The number is 5");
+			break;
+			case 6:
+			printf("The number is 6");
+			break;
+			case 7:
+			printf("The number is 7");
+			break;
+			case 8:
+			printf("The number is 8");
+			break;
+			case 9:
+			printf("The number is 9");
+			break;
+			case 10:
+			printf("The number is 10");
+			break;
+			case 11:
+			printf("The number is 11");
+			break;
+			case 12:
+			printf("The number is 12");
+			break;
+			case 13:
+			printf("The number is 13");
+			break;
+			case 14:
+			printf("The number is 14");
+			break;
+			case 15:
+			printf("The number is 15");
+			break;
+			case 16:
+			printf("The number is 16");
+			break;
+			case 17:
+			printf("The number is 17");
+			break;
+			case 18:
+			printf("The number is 18");
+			break;
+			case 19:
+			printf("The number is 19");
+			break;
+			case 20:
+			printf("The number is 20");
+			break;
+			
+	}
+	return 0;
+}
